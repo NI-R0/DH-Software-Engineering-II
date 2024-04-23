e349b4d35c510f119a1e9c286d6406320bca2c80
Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>
2024-04-23 17:54:56 +0200

    Initialized DTO and Service classes
    Signed-off-by: Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>

---
+++ src/backend/adapters/src/main/java/de/dhbw/softwareengineering/account/AccountDtoToAggregateMapper.java
@@ -0,0 +1,4 @@
+package de.dhbw.softwareengineering.account;
+
+public class AccountDtoToAggregateMapper {
+}
---
+++ src/backend/adapters/src/main/java/de/dhbw/softwareengineering/transaction/TransactionDtoToEntityMapper.java
@@ -0,0 +1,4 @@
+package de.dhbw.softwareengineering.transaction;
+
+public class TransactionDtoToEntityMapper {
+}
