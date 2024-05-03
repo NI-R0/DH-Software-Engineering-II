d3a12369b2cb31a0d5c4c698d5fa91c7b73b05f2
Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>
2024-05-03 11:31:09 +0200

    initialized remaining API methods
    Signed-off-by: Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>

--- src/backend/plugins/src/main/java/de/dhbw/softwareengineering/plugins/REST/InstitutionController.java
+++ src/backend/plugins/src/main/java/de/dhbw/softwareengineering/plugins/REST/InstitutionController.java
@@ -3,6 +3,7 @@ package de.dhbw.softwareengineering.plugins.REST;
 import de.dhbw.softwareengineering.adapters.account.AccountDto;
 import de.dhbw.softwareengineering.adapters.institution.InstitutionCreationDto;
 import de.dhbw.softwareengineering.adapters.institution.InstitutionDto;
+import de.dhbw.softwareengineering.application.AccountService;
 import de.dhbw.softwareengineering.application.InstitutionService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -18,34 +19,36 @@ import java.util.UUID;
 @RestController
 public class InstitutionController {
     @Autowired
-    InstitutionService service;
+    InstitutionService institutionService;
+    @Autowired
+    AccountService accountService;
 
-    @GetMapping("/institutions/getAll")
+    @GetMapping("/api/institutions/getAll")
     @Tag(name = "Institution")
     public ResponseEntity<List<InstitutionDto>> getAllInstitutions(){
-        return ResponseEntity.ok(service.getAllInstitutions());
+        return ResponseEntity.ok(institutionService.getAllInstitutions());
     }
 
-    @GetMapping("/institutions/getInstitution/id={id}")
+    @GetMapping("/api/institutions/getInstitution/id={id}")
     @Tag(name = "Institution")
     public ResponseEntity<InstitutionDto> getInstitutionById(@PathVariable UUID id){
-        Optional<InstitutionDto> dto = service.getInstitutionById(id);
+        Optional<InstitutionDto> dto = institutionService.getInstitutionById(id);
         return dto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/institutions/getInstitution/name={name}")
+    @GetMapping("/api/institutions/getInstitution/name={name}")
     @Tag(name = "Institution")
     public ResponseEntity<InstitutionDto> getInstitutionByName(@PathVariable String name){
-        Optional<InstitutionDto> dto = service.getInstitutionByName(name);
+        Optional<InstitutionDto> dto = institutionService.getInstitutionByName(name);
         return dto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 
-    @PostMapping("/institutions/createInstitution")
+    @PostMapping("/api/institutions/createInstitution")
     @Tag(name = "Institution")
     public ResponseEntity<InstitutionDto> createInstitution(@RequestBody InstitutionDto dto){
         try{
-            Optional<InstitutionDto> body = service.createInstitution(dto);
+            Optional<InstitutionDto> body = institutionService.createInstitution(dto);
             return ResponseEntity.ok(body.orElseThrow(IllegalArgumentException::new));
         }
         catch(Exception e){
@@ -53,11 +56,11 @@ public class InstitutionController {
         }
     }
 
-    @PutMapping("/institutions/updateInstitution")
+    @PutMapping("/api/institutions/updateInstitution")
     @Tag(name = "Institution")
     public ResponseEntity<InstitutionDto> updateInstitution(@RequestBody InstitutionDto dto){
         try{
-            Optional<InstitutionDto> body = service.updateInstitution(dto);
+            Optional<InstitutionDto> body = institutionService.updateInstitution(dto);
             return ResponseEntity.ok(body.orElseThrow(IllegalArgumentException::new));
         }
         catch(Exception e){
@@ -65,11 +68,11 @@ public class InstitutionController {
         }
     }
 
-    @DeleteMapping("/institutions/deleteInstitution/id={id}")
+    @DeleteMapping("/api/institutions/deleteInstitution/id={id}")
     @Tag(name = "Institution")
     public ResponseEntity<Void> deleteById(@PathVariable UUID id){
         try{
-            service.deleteInstitutionById(id);
+            institutionService.deleteInstitutionById(id);
             return ResponseEntity.ok().build();
         }
         catch(Exception e){
@@ -77,11 +80,11 @@ public class InstitutionController {
         }
     }
 
-    @DeleteMapping("/institutions/deleteInstitution/name={name}")
+    @DeleteMapping("/api/institutions/deleteInstitution/name={name}")
     @Tag(name = "Institution")
     public ResponseEntity<Void> deleteByName(@PathVariable String name){
         try{
-            service.deleteInstitutionByName(name);
+            institutionService.deleteInstitutionByName(name);
             return ResponseEntity.ok().build();
         }
         catch(Exception e){
@@ -89,36 +92,42 @@ public class InstitutionController {
         }
     }
 
-
+    ////////////////////////////////////////////////////////////////////////////////////////////
     //Account methods
 
-    @GetMapping("/institutions/{institutionName}/accounts/getAll")
+    @GetMapping("/api/institution={institutionName}/accounts/getAll")
     @Tag(name = "Account")
     public ResponseEntity<List<AccountDto>> getAllAccounts(@PathVariable String institutionName){
         return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/institutions/{institutionName}/accounts/createAccount")
+    @PostMapping("/api/institution={institutionName}/accounts/createAccount")
     @Tag(name = "Account")
-    public ResponseEntity<InstitutionDto> createAccount(@PathVariable String institutionName, @RequestBody AccountDto dto){
+    public ResponseEntity<InstitutionDto> createAccount(@PathVariable String institutionName,
+                                                        @RequestBody AccountDto dto){
         return ResponseEntity.ok().build();
     }
 
-    @PutMapping("/institutions/{institutionName}/accounts/updateAccount")
+    @PutMapping("/api/institution={institutionName}/accounts/updateAccount")
     @Tag(name = "Account")
-    public ResponseEntity<InstitutionDto> updateAccount(@PathVariable String institutionName, @RequestBody AccountDto dto){
+    public ResponseEntity<InstitutionDto> updateAccount(@PathVariable String institutionName,
+                                                        @RequestBody AccountDto dto){
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/institutions/{institutionName}/accounts/deleteAccount/id={accountId}")
+    @DeleteMapping("/api/institution={institutionName}/accounts/deleteAccount/id={accountId}")
     @Tag(name = "Account")
-    public ResponseEntity<InstitutionDto> deleteAccountById(@PathVariable String institutionName,@PathVariable UUID accountId, @RequestBody AccountDto dto){
+    public ResponseEntity<InstitutionDto> deleteAccountById(@PathVariable String institutionName,
+                                                            @PathVariable UUID accountId,
+                                                            @RequestBody AccountDto dto){
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/institutions/{institutionName}/accounts/deleteAccount/name={accountName}")
+    @DeleteMapping("/api/institution={institutionName}/accounts/deleteAccount/name={accountName}")
     @Tag(name = "Account")
-    public ResponseEntity<InstitutionDto> deleteAccountByName(@PathVariable String institutionName,@PathVariable String accountName, @RequestBody AccountDto dto){
+    public ResponseEntity<InstitutionDto> deleteAccountByName(@PathVariable String institutionName,
+                                                              @PathVariable String accountName,
+                                                              @RequestBody AccountDto dto){
         return ResponseEntity.ok().build();
     }
 
@@ -137,4 +146,40 @@ public class InstitutionController {
     public ResponseEntity<Tutorial> getTutorialById(@PathVariable("id") long id) {
 
     }*/
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //Transaction Methods
+
+    @GetMapping("/api/institution={institutionName}/account={accountName}/transactions/getAllTransactions")
+    @Tag(name = "Transaction")
+    public ResponseEntity<List<AccountDto>> getAllTransactions(@PathVariable String institutionName,
+                                                               @PathVariable String accountName){
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/api/institution={institutionName}/account={accountName}/transactions/createTransaction")
+    @Tag(name = "Transaction")
+    public ResponseEntity<InstitutionDto> createTransaction(@PathVariable String institutionName,
+                                                            @PathVariable String accountName,
+                                                            @RequestBody AccountDto dto){
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/api/institution={institutionName}/account={accountName}/transactions/updateTransaction")
+    @Tag(name = "Transaction")
+    public ResponseEntity<InstitutionDto> updateTransaction(@PathVariable String institutionName,
+                                                            @PathVariable String accountName,
+                                                            @RequestBody AccountDto dto){
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/api/institution={institutionName}/account={accountName}/transactions/deleteTransaction/id={transactionId}")
+    @Tag(name = "Transaction")
+    public ResponseEntity<InstitutionDto> deleteTransaction(@PathVariable String institutionName,
+                                                            @PathVariable String accountName,
+                                                            @PathVariable UUID transactionId,
+                                                            @RequestBody AccountDto dto){
+        return ResponseEntity.ok().build();
+    }
 }
