a032f9899822a63fc258595688166d36057fb6d7
Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>
2024-05-05 13:39:28 +0200

    Implemented first REST method
    Signed-off-by: Niklas Rodenbüsch <98448863+NI-R0@users.noreply.github.com>

--- src/backend/0-plugins/src/main/java/de/dhbw/softwareengineering/plugins/REST/InstitutionController.java
+++ src/backend/0-plugins/src/main/java/de/dhbw/softwareengineering/plugins/REST/InstitutionController.java
@@ -1,4 +1,36 @@
 package de.dhbw.softwareengineering.plugins.REST;
 
+import de.dhbw.softwareengineering.adapters.institution.InstitutionDTO;
+import de.dhbw.softwareengineering.adapters.institution.InstitutionToDTOMapper;
+import de.dhbw.softwareengineering.application.InstitutionApplicationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/api/institutions")
 public class InstitutionController {
+
+    private final InstitutionApplicationService institutionService;
+
+    private final InstitutionToDTOMapper institutionMapper;
+
+    @Autowired
+    public InstitutionController(InstitutionApplicationService institutionService, InstitutionToDTOMapper institutionMapper) {
+        this.institutionService = institutionService;
+        this.institutionMapper = institutionMapper;
+    }
+
+    @GetMapping("/getAll")
+    public List<InstitutionDTO> getAllInstitutions(){
+        return this.institutionService.findAllInstitutions()
+                .stream()
+                .map(institutionMapper)
+                .collect(Collectors.toList());
+    }
+
 }
--- src/backend/2-application/src/main/java/de/dhbw/softwareengineering/application/InstitutionApplicationService.java
+++ src/backend/2-application/src/main/java/de/dhbw/softwareengineering/application/InstitutionApplicationService.java
@@ -1,4 +1,23 @@
 package de.dhbw.softwareengineering.application;
 
+import de.dhbw.softwareengineering.domain.institution.Institution;
+import de.dhbw.softwareengineering.domain.institution.InstitutionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class InstitutionApplicationService {
+
+    private final InstitutionRepository institutionRepository;
+
+    @Autowired
+    public InstitutionApplicationService(InstitutionRepository institutionRepository) {
+        this.institutionRepository = institutionRepository;
+    }
+
+    public List<Institution> findAllInstitutions(){
+        return this.institutionRepository.findAllInstitutions();
+    }
 }
